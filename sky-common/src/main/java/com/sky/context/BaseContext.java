b99cb6c4a34e62f8022956f77039573db4dbb723
beacon776 <3214984261@qq.com>
2025-04-03 16:35:02 +0800

    员工分页查询

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -2,16 +2,20 @@ package com.sky.context;
 
 public class BaseContext {
 
-    public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
+    // 静态ThreadLocal变量，存储线程独立的Long类型值
+    private static final ThreadLocal<Long> threadLocal = new ThreadLocal<>();
 
+    // 设置当前线程的ID
     public static void setCurrentId(Long id) {
         threadLocal.set(id);
     }
 
+    // 获取当前线程的ID
     public static Long getCurrentId() {
         return threadLocal.get();
     }
 
+    // 移除当前线程的ID
     public static void removeCurrentId() {
         threadLocal.remove();
     }
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,8 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.dto.EmployeeDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -17,8 +19,20 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+	/**
+	 * 插入员工数据
+	 * @param employee
+	 */
 	@Insert("INSERT INTO employee(id_number, name, phone, sex, username, password, create_time, update_time, create_user, update_user) " +
 			"VALUES(#{idNumber}, #{name}, #{phone}, #{sex}, #{username}, #{password}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
 	void insert(Employee employee);
+
+	/**
+	 * 分页查询
+	 * @param employeePageQueryDTO
+	 * @return
+	 */
+	Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
 
 }
