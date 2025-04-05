77be1d2b48e83ebd604eb589fc58a72766751e62
beacon776 <3214984261@qq.com>
2025-04-05 16:43:15 +0800

    菜品分页查询

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,8 +1,13 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
 
 public interface DishService {
 
 	public void saveWithFlavor(DishDTO dishDTO);
+
+	PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
